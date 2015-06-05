a309650e7d079b3c3fd84e2afa1cc9e305a0fd86
OopsOutOfMemory <victorshengli@126.com>
2015-06-05 14:57:07 +0800

    fix style

--- sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
@@ -40,7 +40,7 @@ class SQLQuerySuite extends QueryTest with BeforeAndAfterAll with SQLTestUtils {
   import sqlContext.implicits._
 
   test("SPARK-8010: promote numeric to string") {
-    val df = Seq((1,1)).toDF("key","value")
+    val df = Seq((1, 1)).toDF("key", "value")
     df.registerTempTable("src")
     val queryCaseWhen = sql("select case when true then 1.0 else '1' end from src ")
     val queryCoalesce = sql("select coalesce(null, 1, '1') from src ")
